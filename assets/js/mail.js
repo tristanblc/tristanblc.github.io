49eecef506be8bf35841e2f0056521d8ab42734d
tristanblc <tblanc.sio@gmail.com>
2024-12-30 09:40:23 +0100

    add ci cd and find to secret tokens on secret token git repo

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -2,21 +2,15 @@
     window.onload = function() {
         document.getElementById('contact-form').addEventListener('submit', function(event) {
             event.preventDefault();
-
-            fetch('./assets/json/app.json')
-                .then(data =>
-                {
-                    var token = data["emailjsApiKey"]
-                    var template = data["templateKey"]
-                    var service = data["serviceKey"]
-
-                    emailjs.sendForm('service_kvlzfvd', 'template_ei2powl', this,'VNhCa88OWBdYf9CHM')
+            const apiKey = process.env.EMAIL_JS_API_KEY
+            const templateId = process.env.TEMPLATE_KEY
+            const serviceId = process.env.SERVICE_KEY
+            emailjs.sendForm(serviceId, templateId, this,apiKey)
                          .then(() => {
                            $('#alert-sucess').show();
                          }, (error) => {
                            $('#alert-danger').show();
                       });
-                })
 
         });
     }
