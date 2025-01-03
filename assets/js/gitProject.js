e9ee9d305ee27e3e8fb137e3db4ea86b7fd76bed
tristanblc <tblanc.sio@gmail.com>
2025-01-03 18:37:55 +0100

    modify  accordon

--- assets/js/gitProject.js
+++ assets/js/gitProject.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     var resultat = [];
+
     $.ajax({
       url: "https://api.github.com/users/tristanblc/repos",
       method: "GET",
@@ -12,21 +13,86 @@ $(document).ready(function () {
             json.description = "Aucune description";
           }   
        
+    
+          var string ='<div class="accordion-item">';
+          string += '<h2 class="accordion-header justify-content-center" id="'+json.name+'">';
+          string +='<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+json.name+'" aria-expanded="false" aria-controls="collapse'+json.name+'">'+json.name+'</button>';
+          string +='</h2>';
+          string +='<div id="collapse'+json.name+'" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#test">';
+          string +='<div class="accordion-body">';
+          string += '<div class="container">'
+          string += '<div class="row">'
+          string  += '      <div class="col-sm-6">';
+          string  += '         <br>';
+          string  += '        <div class="card">';
+          string  += '          <div class="card-body" id="card'+json.name+'">';
+          string  += '            <h5 class="card-title">'+json.name+'</h5>';
+          string  += '            <p class="card-text">'+json.description+'<p>';
+          string  += '            <a href="'+json.svn_url+'" class="card-link btn btn-primary">Lien sur github 🌐</a>';
+          string  += '          </div>';
+          string  += '          <br>';
+          string  += '       </div>'; 
+          string  += '      </div>';
+          string  += '      <div class="col-sm-6">';
+          string  += '      <br>'
+          string  += '      <div class="card">';
+   
+          string  += '          <div class="card-body" id="card'+json.name+'_2">'; 
+          string += ' <h5 class="card-title"> Langages de programmation : </h5>';
+          <br></br> 
+          $.ajax({
+            url: "https://api.github.com/repos/tristanblc/"+json.name+"/languages",
+            method: "GET",
+            dataType: "json",
+            success: function(res) {
+              
+              const jsonProject = JSON.parse(JSON.stringify(res))	
+              for (var key in jsonProject){
+                $('#card'+json.name+'_2').append('<p class="card-title">'+key+'</p>');
+                string  += '';   
+              }
+              
+            },
+            error: function (request, status, error) {
+              $('#test').append('<div class="alert alert-danger" role="alert">Probleme de chargement des projets</div>')
         
-          $('#test').append(' <div class="accordion-item"><h2 class="accordion-header" id="'+json.name+'"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+json.name+'" aria-expanded="false" aria-controls="collapse'+json.name+'">'+json.name+'</button></h2><div id="collapse'+json.name+'" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#test"><div class="accordion-body"><strong>'+json.description+'</strong><h5 class="card-title">'+json.name+'</h5><em>'+json.description+'</em></p><a href="'+json.svn_url+'" class="btn btn-primary">Lien sur github 🌐</a><br></div></div></div></div></div></div>')
+            }
+          })
+
+ 
+          string  += '          </div>';
+          string  += '          <br>';
+          string  += '       </div>';
+          string  += '      </div>';
+          string  += '      </div>';
+         
+          string += '</div>';
+            string  += '</div>';
+            string  += '</div>';
+            string  += '</div>';
+            string  += '</div>';
+            string  += '</div>';
+            $('#test').append(string);
+
+
+
             
             $.ajax({
-            url : "https://api.github.com/repos/"+json.full_name+"",
+            url : "https://api.github.com/repos/"+json.full_name+"/tags",
             method: "GET",
             dataType: "json",
              success: function(resultat) {
                       
                           const jsonProject = JSON.parse(JSON.stringify(resultat))	
-                          print(jsonProject[0]["languages"])    	   
-                          $('div[id="collapse'+json.name+'"]').append('<br><a href="'+jsonProject[0].zipball_url+'" class="btn btn-primary" style="margin-right">Télécharger le projet 📲</a> <br>');
+                              	   
+                          $('div[id="card'+json.name+'"]').append('<br><a href="'+jsonProject[0].zipball_url+'" class="btn btn-primary" style="margin-right">Télécharger le projet 📲</a> <br>');
            
               
               
+           },
+           error: function (request, status, error) {
+              $('#test').append('<div class="alert alert-danger" role="alert">Probleme de chargement des projets</div>')
+        
            }
           });
       
@@ -35,13 +101,19 @@ $(document).ready(function () {
         
        
       }
-     
-       
+      
   
       
+      },
+      error: function (request, status, error) {
+        $('#test').append('<div class="alert alert-danger" role="alert">Probleme de chargement des projets</div>')
+  
       }
       
-      });
-
+      },
+      
+    );
+    
   
-  });
+  })
+  ;
